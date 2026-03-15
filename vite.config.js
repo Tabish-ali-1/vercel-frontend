c35794552db0e050f5c32ca94a79abeda1aa1f19
tabish ali <ta3446896@gmail.com>
2026-03-16 00:59:03 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base:process.env.VITE_BASE_PATH || "/vercel-frontend",
+  server: { port: 5173,
+    proxy :{
+      '/api':{
+        target:'https://vercel-backend-zbs3.onrender.com',
+        changeOrigin:true,
+      },
+    },
+   },
 });
